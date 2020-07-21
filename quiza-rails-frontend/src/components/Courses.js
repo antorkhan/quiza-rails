b338dd96969ef61811c1294213bd23317670bd39
MD Tawab Alam Khan <cs.antorkhan@gmail.com>
2020-07-21 18:54:37 +0600

    Setup React Router DOM

---
+++ quiza-rails-frontend/src/components/Courses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {} from 'react-router-dom'
+
+
+
+function Courses() {
+  return (
+    <div>
+        This is Test
+    </div>
+  );
+}
+
+export default Courses;
---
+++ quiza-rails-frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {} from 'react-router-dom'
+
+
+
+function Home() {
+  return (
+    <div>
+        This is home
+    </div>
+  );
+}
+
+export default Home;
