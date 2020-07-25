b5207cb5d77425d0d7a8b366ac44ddd4498c3987
MD Tawab Alam Khan <cs.antorkhan@gmail.com>
2020-07-26 04:25:05 +0600

    Created Seed File

--- quiza-rails-backend/db/seeds.rb
+++ quiza-rails-backend/db/seeds.rb
@@ -5,4 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-sys_user = SysUser.create(username: "antor", password: "123123", name: 'Antor Khan')
+course = Course.create(name: 'Geography 101', description: 'First Geography Course')
+lesson_1 = course.lessons.create(name: 'Mid Term Lessons', description: 'Geography Term Geography Lesson Description')
+lesson_1.questions.create(text: 'Which one is the capital of Bangladesh?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'Dhaka', correct_option: 4)
+lesson_1.questions.create(text: 'Which one is the capital of India?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Delhi', correct_option: 4)
+lesson_1.questions.create(text: 'Which one is the capital of Pakistan?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Karachi', correct_option: 4)
+
+lesson_2 = course.lessons.create(name: 'Final Term Lessons', description: 'Geography Term Geography Lesson Description')
+lesson_2.questions.create(text: 'Which one is the capital of Bangladesh?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'Dhaka', correct_option: 4)
+lesson_2.questions.create(text: 'Which one is the capital of India?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Delhi', correct_option: 4)
+lesson_2.questions.create(text: 'Which one is the capital of Pakistan?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Karachi', correct_option: 4)
+
+course = Course.create(name: 'Social 102', description: 'Second Social Course')
+lesson_1 = course.lessons.create(name: 'Mid Term Lessons', description: 'Social Term Social Lesson Description')
+lesson_1.questions.create(text: 'Which one is the capital of Bangladesh?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'Dhaka', correct_option: 4)
+lesson_1.questions.create(text: 'Which one is the capital of India?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Delhi', correct_option: 4)
+lesson_1.questions.create(text: 'Which one is the capital of Pakistan?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Karachi', correct_option: 4)
+
+lesson_2 = course.lessons.create(name: 'Final Term Lessons', description: 'Social Term Social Lesson Description')
+lesson_2.questions.create(text: 'Which one is the capital of Bangladesh?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'Dhaka', correct_option: 4)
+lesson_2.questions.create(text: 'Which one is the capital of India?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Delhi', correct_option: 4)
+lesson_2.questions.create(text: 'Which one is the capital of Pakistan?', option_1: 'Noakhali', option_2: 'Barisal', option_3: 'Chandpur', option_4: 'New Karachi', correct_option: 4)
